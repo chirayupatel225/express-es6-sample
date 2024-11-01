7567505b2ec9d2328d74f1883f7f94e793831865
Chirayu Patel <59722487+chirayupatel225@users.noreply.github.com>
2024-11-01 12:25:13 +0800

    Update index.js (Added Student ID)

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'World' });
+  res.render('index', { title: 'My STUDENT ID is 21525649 - Chirayu' });
 });
 
 export default router;
